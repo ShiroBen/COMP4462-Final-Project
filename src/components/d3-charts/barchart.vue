f1506eee11543cacacc30308cab2ec23e271a471
User <1917728805@qq.com>
2024-11-19 22:08:06 +0800

    task1 bar chart

---
+++ src/components/d3-charts/barchart.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <h1>Bar charts for Popular Music Genres</h1>
+    <!-- // <div ref="radarGraphContainer" style="width: 600px; height: 600px;"></div> -->
+    <iframe width="100%" height="384.171875" frameborder="0"
+  src="https://observablehq.com/embed/66ebda84a4824705@375?cells=chart"></iframe>
+</template>
+<script setup>
+import * as Plot from "@observablehq/plot";
+import { defineComponent, ref, onMounted,h } from 'vue';
+import * as d3 from 'd3';
+
+
+</script>
