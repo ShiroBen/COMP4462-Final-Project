0f391640294458bb4b377339726fb32e49c698da
User <1917728805@qq.com>
2024-11-22 19:43:46 +0800

    task 1 modification

--- src/components/d3-charts/radarChart.vue
+++ src/components/d3-charts/radarChart.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>Radar graph for analyzing Music Styles</h1>
     <!-- // <div ref="radarGraphContainer" style="width: 600px; height: 600px;"></div> -->
-    <iframe width="100%" height="803.5625" frameborder="0"
-    src="https://observablehq.com/embed/c3189374362b7fcb@557?cells=viewof+chosenTrack%2CradarChart"></iframe>
+    <iframe width="100%" height="804.5625" frameborder="0"
+  src="https://observablehq.com/embed/c3189374362b7fcb@575?cells=viewof+chosenTrack%2CradarChart"></iframe>
 </template>
 <script setup>
 // import * as Plot from "@observablehq/plot";
--- src/components/d3-charts/scatterPlot.vue
+++ src/components/d3-charts/scatterPlot.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>Scatter Plot for relationships between track features</h1>
     <!-- // <div ref="radarGraphContainer" style="width: 600px; height: 600px;"></div> -->
-    <iframe width="100%" height="1027" frameborder="0"
-  src="https://observablehq.com/embed/91aa726cf4414c7a?cells=viewof+propertyOne%2Cviewof+propertyTwo%2Cviewof+isExplicit%2Cviewof+Mode%2Cviewof+YearRange%2Cviewof+PopularityRange%2CscatterPlot"></iframe>
+    <iframe width="100%" height="1028.59375" frameborder="0"
+  src="https://observablehq.com/embed/ffe038def9705f7d?cells=viewof+propertyOne%2Cviewof+propertyTwo%2Cviewof+isExplicit%2Cviewof+Mode%2Cviewof+YearRange%2Cviewof+PopularityRange%2CscatterPlot"></iframe>
 </template>
 <script setup>
 // import * as Plot from "@observablehq/plot";
